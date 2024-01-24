aa9d6d83647881e34e1fa5c430008452605ebf6a
Mason <mason_mchugh2@baylor.edu>
2024-01-24 14:42:45 -0600

    updated gitignore and username

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,13 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
+
+#Step 1 is to create a virtual Enviroment - PC use py -3 -m venv (name of virtual enviroment)
+
+#Step 2 is to activate the vitrual enviroment - PC use myvenv/Scripts/activate
+
+#Step 3 is to install 3rd party library/module starts with pip3 install (library name)
+
+#To ignore files and folders, create a special file
